dea7e2141b596d6d9597607545ccd9f1c1f2858e
Ray Climie <seederwood@gmail.com>
2017-07-20 01:54:35 -0300

    edit update and delete methods and pages

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,7 +1,9 @@
 class PicturesController < ApplicationController
-  before_action :find_picture, only: %i[show edit destroy]
+  before_action :find_picture, only: %i[show edit destroy update]
 
-  def index; end
+  def index
+    @pictures = Picture.all.order('created_at desc')
+  end
 
   def show; end
 
@@ -13,11 +15,27 @@ class PicturesController < ApplicationController
     @picture = Picture.new(picture_params)
 
     if @picture.save
-      redirect_to @picture, notice: 'Post @picture.title was created successfully!'
+      redirect_to @picture, notice: "Post '#{@picture.title}' was created successfully!"
     else
       render 'new'
     end
   end
+
+  def edit; end
+
+  def update
+    if @picture.update(picture_params)
+      redirect_to @picture, notice: "Post '#{@picture.title}' was updated successfully!"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @picture.destroy
+    redirect_to root_path, notice: "Post was deleted successfully!"
+  end
+  
 
   private
 
