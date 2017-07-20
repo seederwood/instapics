ac0b1b64dacba3108d89018b811b2d8d91e88ecf
Ray Climie <seederwood@gmail.com>
2017-07-20 00:52:13 -0300

    create new ppics files and views

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,3 +1,17 @@
 class PicturesController < ApplicationController
   def index; end
+
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+    @picture = Picture.new(picture_params)
+  end
+
+  private
+
+  def picture_params
+    params.require(:picture).permit(:title, :description)
+  end
 end
