3161aef3c871e1888db207d7e43724dbe1b6b4ef
Ray Climie <seederwood@gmail.com>
2017-07-20 01:12:52 -0300

    show and index pages

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,9 @@
 class PicturesController < ApplicationController
+  before_action :find_picture, only: %i[show edit destroy]
+
   def index; end
+
+  def show; end
 
   def new
     @picture = Picture.new
@@ -7,11 +11,21 @@ class PicturesController < ApplicationController
 
   def create
     @picture = Picture.new(picture_params)
+
+    if @picture.save
+      redirect_to @picture, notice: 'Post @picture.title was created successfully!'
+    else
+      render 'new'
+    end
   end
 
   private
 
   def picture_params
     params.require(:picture).permit(:title, :description)
+  end
+
+  def find_picture
+    @picture = Picture.find(params[:id])
   end
 end
